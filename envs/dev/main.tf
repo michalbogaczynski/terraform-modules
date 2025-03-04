f815f474a44ff5dcfe8ceb8df5147d067c09e1e8
michalbogaczynski <bogaczynski.m@gmail.com>
2025-03-04 21:42:33 +0100

    created vnet on azure

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,5 +1,23 @@
 module "resource-group" {
-  source = "./modules/resource-group"
+  source = "../../modules/resource-group"
   resource_group_location="Poland Central"  
   resource_group_name="DEVOPS"
+}
+
+
+module "vnet" {
+  source              = "../../modules/vnet/"
+  name                = var.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = var.address_space
+  resource_group_name = azurerm_resource_group.rg.name
+
+  subnets = {
+    "dev-subnet-01" = {
+      address_prefix = "10.0.3.0/24"
+    }
+    "dev-subnet-02" = {
+      address_prefix = "10.0.4.0/24"
+    }
+  }
 }
---
+++ modules/vnet/main.tf
@@ -0,0 +1,40 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.name}-vnet"
+  address_space       = var.address_space
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "private_subnet" {
+  for_each             = var.private_subnets
+  name                 = each.key
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [each.value.address_prefix]
+}
+
+resource "azurerm_subnet" "public_subnet" {
+  for_each             = var.public_subnets
+  name                 = each.key
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [each.value.address_prefix]
+}
+
+resource "azurerm_subnet" "public_database_subnet" {
+  for_each             = var.public_database_subnets
+  name                 = each.key
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [each.value.address_prefix]
+
+  delegation {
+    name = "mysql-delegation"
+    service_delegation {
+      name = "Microsoft.DBforMySQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}
